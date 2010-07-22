6b8886945db0e665922a9f1e35f4bb61e86d603a
Damon Oehlman <damon.oehlman@gmail.com>
2010-07-22 12:11:41 +1000

    Checkin for Liam to review

--- js/prowebapps.js
+++ js/prowebapps.js
@@ -1,10 +1,12 @@
 PROWEBAPPS = (function() {
+    var actionCounter = 0;
     var ROTATION_CLASSES = {
         "0": "none",
         "90": "right",
         "-90": "left",
         "180": "flipped"
     };
+    
     
     function monitorOrientationChanges() {
         var canDetect = "onorientationchange" in window;
@@ -36,6 +38,183 @@ PROWEBAPPS = (function() {
     } // monitorOrientationChanges
     
     var module = {
+        getFormValues: function(form) {
+            var values = {};
+            
+            // iterate through the form values using jQuery
+            jQuery(form).find(":input").each(function() {
+                // get the name of the field
+                var fieldName = this.name.replace(/^.*\[(\w+)\]$/, "$1");
+                
+                // set the value for the field
+                values[fieldName] = this.value;
+            });
+            
+            return values;
+        },
+        
+        Storage: (function() {
+            window.addEventListener("storage", function(evt) {
+                alert(evt);
+            }, false);
+            
+            function getStorageScope(scope) {
+                if (scope && (scope == "session")) {
+                    return sessionStorage;
+                } // if
+                
+                return localStorage;
+            } // getStorageTarget
+            
+            // TODO: check for storage support here
+            
+            return {
+                get: function(key, scope) {
+                    // get the storage target
+                    var value = getStorageScope(scope).getItem(key);
+                    
+                    // if the value looks like serialized JSON, parse it
+                    return (/^(\{|\[).*(\}|\])$/).test(value) ? JSON.parse(value) : value;
+                },
+                
+                set: function(key, value, scope) {
+                    // if the value is an object, the stringify using JSON
+                    var serializable = jQuery.isArray(value) || jQuery.isPlainObject(value);
+                    var storeValue = serializable ? JSON.stringify(value) : value;
+                    
+                    // save the value
+                    getStorageScope(scope).setItem(key, storeValue);
+                },
+                
+                remove: function(key, scope) {
+                    getStorageScope(scope).removeItem(key);
+                }
+            };
+        })(),
+        
+        ViewAction: function(args) {
+            args = jQuery.extend({
+                label: "",
+                run: null
+            }, args);
+            
+            var self = {
+                id: actionCounter++,
+                
+                getAnchor: function() {
+                    return "<a href='#' id='action_" + self.id + "'>" + args.label + "</a>";
+                },
+                
+                execute: function() {
+                    if (args.run) {
+                        args.run.apply(this, arguments);
+                    } // if
+                }
+            };
+            
+            return self;
+        },
+        
+        ChangeViewAction: function(args) {
+            // if the target is not defined, then raise an error
+            if (! args.target) {
+                throw new Error("Unable to create a ChangeViewAction without a target specified.");
+            } // if
+            
+            // prep the label to equal the target if not defined
+            if (! args.label) {
+                args.label = args.target;
+            } // if
+            
+            return new module.ViewAction(jQuery.extend({
+                run: function() {
+                    module.ViewManager.activate(args.target);
+                }
+            }, args));
+        },
+        
+        ViewManager: (function() {
+            var views = {};
+            var activeView = null;
+            
+            function switchView(oldView, newView) {
+                var ii, menu = jQuery("#menu").get(0);
+                
+                // switch the views
+                oldView ? jQuery("#" + oldView.id).hide() : null;
+                newView ? jQuery("#" + newView.id).show() : null;
+                
+                // if we have a menu, then update the actions
+                if (menu) {
+                    // clear the menu and create list items and anchors as required 
+                    jQuery(menu).html('');
+                    for (ii = 0; activeView && (ii < activeView.actions.length); ii++) {
+                        jQuery(menu).append("<li>" + activeView.actions[ii].getAnchor() + "</li>");
+                    } // for
+                    
+                    // attach a click handler to each of the anchors now in the menu
+                    jQuery(menu).find("a").click(function(evt) {
+                        // find the specified view in the active view and execute it
+                        for (ii = 0; ii < activeView.actions.length; ii++) {
+                            if (("action_" + activeView.actions[ii].id) == this.id) {
+                                activeView.actions[ii].execute();
+                                evt.preventDefault();
+                                break;
+                            } // if
+                        } // for
+                    });
+                } // if
+            } // switchView
+            
+            var subModule = {
+                activate: function(viewId) {
+                    // save the old view
+                    var oldView = activeView;
+                    
+                    // if a view id has been specified, but doesn't exist in the views check for a div
+                    if (viewId && (! views[viewId]) && (jQuery("#" + viewId).get(0))) {
+                        subModule.define({
+                            id: viewId
+                        });
+                    } // if
+                    
+                    // update the active view
+                    activeView = viewId ? views[viewId] : null;
+                    
+                    // update the associated ui elements
+                    switchView(oldView, activeView);
+                },
+                
+                getActiveView: function() {
+                    return activeView ? jQuery("#" + activeView.id) : null;
+                },
+                
+                define: function(args) {
+                    args = jQuery.extend({
+                        id: '',
+                        actions: []
+                    }, args);
+                    
+                    // if the id is specified, add the view to the list of defined views
+                    if (args.id) {
+                        views[args.id] = args;
+                    } // if
+                }
+            };
+            
+            $(document).ready(function() {
+                subModule.activate("main");
+                
+                $("a.changeview").each(function() {
+                    $(this).click(function(evt) {
+                        subModule.activate(this.href.replace(/^.*\#(.*)$/, "$1"));
+                        evt.preventDefault();
+                    }); // click
+                });
+            });
+            
+            return subModule;
+        })()
     };
     
     monitorOrientationChanges();
@@ -43,6 +222,7 @@ PROWEBAPPS = (function() {
     // set autoscrolling to hide the URL bar
     $(document).ready(function() {
         window.scrollTo(0, 1);
+        document.location = "#";
     });
     
     return module;
---
+++ snippets/04/todolist.js
@@ -0,0 +1,237 @@
+TODOLIST = (function() {
+    var MILLISECONDS_TO_DAYS = 86400000;
+    
+    // define the module
+    var module = {
+
+        /* todo item */
+        
+        ToDoItem: function(params) {
+            params = jQuery.extend({
+                name: "",
+                description: "",
+                due: null
+            }, params);
+            
+            // initialise self
+            var self = {
+                id: null,
+                name: params.name,
+                description: params.description,
+                due: params.due ? new Date(params.due) : null,
+                
+                getDaysDue: function() {
+                    return Math.floor((self.due - new Date()) / MILLISECONDS_TO_DAYS);
+                }
+            };
+            
+            return self;
+        },
+        
+        /* storage module */
+        
+        Storage: (function() {
+            // open / create a database for the application (expected size ~ 100K)
+            var db = null;
+            
+            try {
+                db = openDatabase("todolist", "1.1", "To Do List Database", 100 * 1024);
+                
+                // check that we have the required tables created
+                db.transaction(function(transaction) {
+                    transaction.executeSql(
+                        "CREATE TABLE IF NOT EXISTS item(" + 
+                        "  name TEXT NOT NULL, " + 
+                        "  description TEXT, " + 
+                        "  due DATETIME, " + 
+                        "  completed DATETIME);");
+                });
+            }
+            catch (e) {
+                 db = openDatabase("todolist", "1.0", "To Do List Database", 100 * 1024);
+
+                // check that we have the required tables created
+                db.transaction(function(transaction) {
+                    transaction.executeSql(
+                        "CREATE TABLE IF NOT EXISTS item(" + 
+                        "  name TEXT NOT NULL, " + 
+                        "  description TEXT, " + 
+                        "  due DATETIME);");
+                });
+                
+                db.changeVersion("1.0", "1.1", function(transaction) {
+                    transaction.executeSql("ALTER TABLE item ADD completed DATETIME;");
+                });
+            }
+            
+            function getSortedItems(compareFn, callback) {
+                subModule.getItems(function(items) {
+                    callback(items.sort(compareFn));
+                });
+            } // getSortedItem
+            
+            var subModule = {
+                getItems: function(callback) {
+                    db.transaction(function(transaction) {
+                        transaction.executeSql(
+                            "SELECT * FROM item",
+                            [],
+                            function (transaction, results) {
+                                // initialise an array to hold the items
+                                var items = [];
+                                
+                                // read each of the rows from the db, and create items
+                                for (var ii = 0; ii < results.rows.length; ii++) {
+                                    items.push(new module.ToDoItem(results.rows.item(ii)));
+                                } // for
+                                
+                                callback(items);
+                            }
+                        );
+                    });
+                },
+                
+                getItemsInPriorityOrder: function(callback) {
+                    getSortedItems(function(itemA, itemB) {
+                        return itemA.due - itemB.due;
+                    }, callback);
+                },
+                
+                getMIT: function(callback) {
+                    subModule.getItemsInPriorityOrder(function(items) {
+                        callback(items.length > 0 ? items[0] : null);
+                    });
+                },
+                
+                saveItem: function(item, callback) {
+                    db.transaction(function(transaction) {
+                        transaction.executeSql(
+                            "INSERT INTO item(name, description, due) VALUES (?, ?, ?);", 
+                            [item.name, item.description, item.due]
+                        );
+                    });
+               }
+            };
+            
+            return subModule;
+        })(),
+        
+        UI: (function() {
+            var subModule = {
+                displayMIT: function(selector, mit) {
+                    var match = jQuery(selector);
+                    
+                    
+                }
+            };
+            
+            return subModule;
+        })(),
+        
+        /* validation module */
+        
+        Validation: (function() {
+            var errors = {};
+
+            return {
+                displayErrors: function(newErrors) {
+                    // initialise variables
+                    var haveErrors = false;
+                    
+                    // update the errors with the new errors
+                    errors = newErrors;
+
+                    // remove the invalid class for all inputs
+                    $(":input.invalid").removeClass("invalid");
+
+                    // iterate through the fields specified in the errors array
+                    for (var fieldName in errors) {
+                        haveErrors = true;
+                        $("input[name='" + fieldName + "']").addClass("invalid");
+                    } // for
+
+                    // if we have errors, then add a message to the errors div
+                    $("#errors")
+                        .html(haveErrors ? "Errors were found." : "")
+                        .css("display", haveErrors ? "block" : "none");
+                },
+                
+                displayFieldErrors: function(field) {
+                    var messages = errors[field.name];
+                    if (messages && (messages.length > 0)) {
+                        // find an existing error detail section for the field
+                        var errorDetail = $("#errordetail_" + field.id).get(0);
+
+                        // if it doesn't exist, then create it
+                        if (! errorDetail) {
+                            $(field).before("<ul class='errors-inline' id='errordetail_" + field.id + "'></ul>");
+                            errorDetail = $("#errordetail_" + field.id).get(0);
+                        } // if
+
+                        // add the various error messages to the div
+                        for (var ii = 0; ii < messages.length; ii++) {
+                            $(errorDetail).html('').append("<li>" + messages[ii] + "</li>");
+                        } // for
+                    } // if
+                } // displayFieldErrors
+            };
+        })()
+    };
+    
+    // define the all tasks view
+    PROWEBAPPS.ViewManager.define({
+        id: "alltasks",
+        actions: [
+            new PROWEBAPPS.ChangeViewAction({
+                target: "add",
+                label: "Add"
+            })
+        ]
+    });
+    
+    return module;
+})();
+
+$(document).ready(function() {
+    $(":input").focus(function(evt) {
+        TODOLIST.Validation.displayFieldErrors(this);
+    }).blur(function(evt) {
+        $("#errordetail_" + this.id).remove();
+    });
+    
+    $("#main .task").
+    
+    TODOLIST.Storage.getMIT(function(mit) {
+        alert("task due on: " + mit.due + " which is " + mit.getDaysDue() + " days away");
+    });
+    
+    $("#taskentry").validate({
+        submitHandler: function(form) {
+            // get the values from the form in hashmap
+            var formValues = PROWEBAPPS.getFormValues(form);
+            
+            // create a new item to save to the database
+            var item = new TODOLIST.ToDoItem(formValues);
+            
+            // now create a new todo list item
+            TODOLIST.Storage.saveItem(item);
+        },
+        showErrors: function(errorMap, errorList) {
+            // initialise an empty errors map
+            var errors = {};
+
+            // iterate through the jQuery validation error map, and convert to 
+            // something we can use
+            for (var elementName in errorMap) {
+                if (! errors[elementName]) {
+                    errors[elementName] = [];
+                } // if
+
+                errors[elementName].push(errorMap[elementName]);
+            } // for
+
+            // now display the errors
+            TODOLIST.Validation.displayErrors(errors);
+        }
+    });
+});
